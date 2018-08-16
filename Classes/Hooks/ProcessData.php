68ad4eba44c1f3b4bfc08d4bdcf8e462ceb134d1
Benjamin Serfhos <benjamin@serfhos.com>
2018-08-16 14:52:29 +0200

    [BUGFIX] Refactored obsolete static function

--- Classes/Hooks/ProcessData.php
+++ Classes/Hooks/ProcessData.php
@@ -66,7 +66,11 @@ class ProcessData
                 $this->locationMapping[$table][$id] = $locationForNewRecord;
             }
 
-            $location = GeoUtility::getLocationData($id, $table);
+            try {
+                $location = $this->getLocationService()->get($id, $table);
+            } catch (AbstractException $e) {
+                $location = null;
+            }
             if ($location === null || $location->getLocation() !== $this->locationMapping[$table][$id]) {
                 $this->getLocationService()->delete($id, $table);
 
